eb67cc5c8f90683ee2055f3d2c31a7dd0d3664a7
Noe Molina <noeml130302@gmail.com>
2026-07-23 20:25:36 -0700

    commit inicial

---
+++ src/main/java/com/NMolina/to_do_list_TT/ToDoListTtApplication.java
@@ -0,0 +1,13 @@
+package com.NMolina.to_do_list_TT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListTtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListTtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NMolina/to_do_list_TT/ToDoListTtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NMolina.to_do_list_TT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListTtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
